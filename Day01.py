aab44bec99203e8fb0cbbfd305a493c083827c35
alijaun514 <muhammadalijaun0514@gmaile.com>
2026-04-04 08:47:31 -0800

    Day 01 python prsctic  basic user input

---
+++ Day01.py
@@ -0,0 +1,26 @@
+print("Hello starting DevOps journey")
+
+print("Hello DevOps jaurney")
+
+print("Ali")
+print("DevOps")
+
+# Task 01
+
+print("I am Ali")
+print("I will become a DevOps")
+
+# Task 02
+name = input("Enter your name ")
+print("Welcome " , name)
+
+# Task 03
+
+age = input("enter yor age: ")
+print("My age is  = " ,  age)
+
+# Self Practic
+
+name = input("entr your name")
+city = input("enter yoyr city ")
+print(name ,"from" ,city)
